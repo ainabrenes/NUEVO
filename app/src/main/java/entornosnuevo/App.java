ba450ea8b6e845808e59f582a769da02098daef3
Aina <abrenes@nigul.cide.es>
2024-03-06 15:06:20 +0100

    actividad3

--- app/src/main/java/entornosnuevo/App.java
+++ app/src/main/java/entornosnuevo/App.java
@@ -7,7 +7,9 @@ public class App {
     public String getGreeting() {
         return "Hello World!";
     }
-
+    public int sumar(int n1, int n2){
+        int suma=n1+n2;
+    }
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
