0456df1816a7fb3279b31bb6f477627e51dcba28
Aina <abrenes@nigul.cide.es>
2024-03-06 15:15:19 +0100

    Suma

--- app/src/main/java/entornosnuevo/App.java
+++ app/src/main/java/entornosnuevo/App.java
@@ -8,7 +8,9 @@ public class App {
         return "Hello World!";
     }
     public int sumar(int n1, int n2){
-        int suma=n1+n2;
+        int suma;
+        suma=n1+n2;
+        return suma;
     }
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
